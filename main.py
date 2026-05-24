521f6b9e099b6e9fd7e192299b9e6146cd51f5ea
luferdias <luis.fernando.dias@gmail.com>
2026-05-24 20:36:16 -0300

    implementado duas novas abordagens de soma

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -8,8 +9,34 @@ def hello_world():
     return {"mensagem": "Hello World"}
 
 
+# http://127.0.0.1:8000/soma/3/2
 # Passando o número 1 e 2 na URL
-@app.get("/soma/{numero1}/{numero2}")
+@app.post("/soma/{numero1}/{numero2}")
 def soma(numero1: int, numero2: int):
     total = numero1 + numero2
+    return {"resultado": total}
+
+
+# http://127.0.0.1:8000/soma_formato2?numero1=3&numero2=2
+@app.post("/soma_formato2")
+def soma_formato2(numero1: int, numero2: int):
+    total = numero1 + numero2
+    return {"resultado": total}
+
+# Passando o número 1 e 2 no corpo da requisição
+class Numeros(BaseModel):
+    numero1: int
+    numero2: int
+
+# 'http://127.0.0.1:8000/soma_formato3' \
+#   -H 'accept: application/json' \
+#   -H 'Content-Type: application/json' \
+#   -d '{
+#   "numero1": 3,
+#   "numero2": 2
+# }'
+
+@app.post("/soma_formato3")
+def soma_formato3(numeros: Numeros):
+    total = numeros.numero1 + numeros.numero2
     return {"resultado": total}
